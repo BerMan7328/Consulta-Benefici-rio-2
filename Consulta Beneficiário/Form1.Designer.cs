fb12dcaa3250a3599a6d5d19323e79dd5bbedab2
BerMan7328 <bmanoelbh@gmail.com>
2020-03-19 09:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ Consulta Beneficiário/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Consulta_Beneficiário
+{
+    partial class Init
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Init));
+            this.butConsult = new System.Windows.Forms.Button();
+            this.butCadastro = new System.Windows.Forms.Button();
+            this.butClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // butConsult
+            // 
+            this.butConsult.Location = new System.Drawing.Point(20, 56);
+            this.butConsult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.butConsult.Name = "butConsult";
+            this.butConsult.Size = new System.Drawing.Size(180, 98);
+            this.butConsult.TabIndex = 0;
+            this.butConsult.Text = "Consulta Beneficiário";
+            this.butConsult.UseVisualStyleBackColor = true;
+            // 
+            // butCadastro
+            // 
+            this.butCadastro.Location = new System.Drawing.Point(397, 56);
+            this.butCadastro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.butCadastro.Name = "butCadastro";
+            this.butCadastro.Size = new System.Drawing.Size(180, 98);
+            this.butCadastro.TabIndex = 1;
+            this.butCadastro.Text = "Cadastro Beneficiário";
+            this.butCadastro.UseVisualStyleBackColor = true;
+            // 
+            // butClose
+            // 
+            this.butClose.Location = new System.Drawing.Point(219, 292);
+            this.butClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.butClose.Name = "butClose";
+            this.butClose.Size = new System.Drawing.Size(128, 52);
+            this.butClose.TabIndex = 2;
+            this.butClose.Text = "Fechar";
+            this.butClose.UseVisualStyleBackColor = true;
+            this.butClose.Click += new System.EventHandler(this.butClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(52, 56);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(493, 276);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Init
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 374);
+            this.Controls.Add(this.butClose);
+            this.Controls.Add(this.butCadastro);
+            this.Controls.Add(this.butConsult);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Init";
+            this.Text = "Plano Certo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butConsult;
+        private System.Windows.Forms.Button butCadastro;
+        private System.Windows.Forms.Button butClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Consulta Beneficiário/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consulta_Beneficiário
+{
+    public partial class Init : Form
+    {
+        public Init()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
