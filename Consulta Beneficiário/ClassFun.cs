95a236992d1ab7236a44ca5024d8dcd5078ed263
BerMan7328 <bmanoelbh@gmail.com>
2020-04-03 11:48:17 -0300

    commit 7

---
+++ Consulta Beneficiário/ClassFun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consulta_Beneficiário
+{
+    public class ClassFun
+    {
+        public string TxtNome { get; set; }
+        public string TxtEnd { get; set; }
+        public string TxtMail { get; set; }
+        public string TxtTel { get; set; }
+        public string TxtCpf { get; set; }
+        public string TxtDN { get; set; }
+        public string TxtCont { get; set; }
+    }
+
+
+}
